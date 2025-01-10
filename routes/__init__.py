d9b8620467a68a3211c7fd9a143209ed2dc81492
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-01-10 21:21:33 +0600

    update routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,11 @@
-from .auth import auth_blueprint
-from .public import public_blueprint
-from .admin import admin_blueprint
-from .order import order_blueprint
+from .public_routes import public_blueprint
+from .user_routes import user_blueprint
+from .admin_routes import admin_blueprint
+from .order_routes import order_blueprint
+
 
 def init_routes(app):
-    app.register_blueprint(auth_blueprint)
     app.register_blueprint(public_blueprint)
     app.register_blueprint(admin_blueprint)
+    app.register_blueprint(user_blueprint)
     app.register_blueprint(order_blueprint)
