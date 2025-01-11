a3a37150a7c08f0d8ab32ae1387961f138b3b22c
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-01-11 13:57:11 +0600

    pass cart information

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
+from flask import session
 from flask import Flask
 from configs.config import Config
 from routes import init_routes
 from configs.database import db
+from models.Cart import Cart
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
-from flask import session
 
 @app.context_processor
 def user():
     return {'username': session.get('username', None), 'is_admin': session.get('is_admin', False)}
+
+
+@app.context_processor
+def inject_cart_count():
+    if 'user_id' in session:
+        cart_count = Cart.query.filter_by(user_id=session['user_id']).count()
+    else:
+        cart_count = 0
+    return {'cart_count': cart_count}
+
 
 init_routes(app)
 
