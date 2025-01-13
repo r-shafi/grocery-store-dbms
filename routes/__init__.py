c103930ecd957aea6d9963e536ee1f67b70dccf2
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-01-13 12:15:26 +0600

    add api blueprint

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,6 +2,7 @@ from .public_routes import public_blueprint
 from .user_routes import user_blueprint
 from .admin_routes import admin_blueprint
 from .order_routes import order_blueprint
+from .api_routes import api_blueprint
 
 
 def init_routes(app):
@@ -9,3 +10,4 @@ def init_routes(app):
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(user_blueprint)
     app.register_blueprint(order_blueprint)
+    app.register_blueprint(api_blueprint)
