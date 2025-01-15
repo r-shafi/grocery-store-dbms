f91c0d5e5f170bdc792f39e4712af4171639a038
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-01-15 19:54:01 +0600

    update category db schema

--- models/Category.py
+++ models/Category.py
@@ -1,27 +1,22 @@
 from datetime import datetime
 from configs.database import db
-from sqlalchemy.orm import validates
 
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True, nullable=False)
+    image = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(
         db.DateTime, default=datetime.now, onupdate=datetime.now)
 
     products = db.relationship('Product', backref='category', lazy=True)
 
-    @validates('name')
-    def validate_name(self, key, value):
-        if not value:
-            raise ValueError("Name cannot be empty")
-        return value
-
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
+            'image': self.image,
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
