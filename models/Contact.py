c16dfebfaea3ab2ce361f8bf18e8c195872628c1
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-01-15 19:16:39 +0600

    create contact database model

---
+++ models/Contact.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from configs.database import db
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    subject = db.Column(db.String(255), nullable=True)
+    message = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Contact {self.name} - {self.email}>'
